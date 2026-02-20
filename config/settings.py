9bf20008c0921e4d7c6bc77f55c56827f08086e6
Akshay Ahuja <ahujaaakshay0000@gmail.com>
2026-02-20 18:46:43 +0530

    feat: add centralized configuration for URLs, API hosts, and environment variables

---
+++ config/settings.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    BROWSERSTACK_USERNAME = os.getenv("BROWSERSTACK_USERNAME")
+    BROWSERSTACK_ACCESS_KEY = os.getenv("BROWSERSTACK_ACCESS_KEY")
+    DEEPL_API_KEY = os.getenv("DEEPL_API_KEY")
+    
+    RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+    RAPIDAPI_HOST = "rapid-translate-multi-traduction.p.rapidapi.com"
+    
+    BASE_URL = "https://elpais.com"
+    OPINION_SECTION_URL = "https://elpais.com/opinion"
+    
+    OUTPUT_DIR = os.path.join(os.getcwd(), "output")
+
+if not os.path.exists(Settings.OUTPUT_DIR):
+    os.makedirs(Settings.OUTPUT_DIR)
